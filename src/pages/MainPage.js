356673af44658ac84ff0f9feb8352fda8824f01e
choi <choi@choiui-MacBook-Pro.local>
2019-04-29 14:11:54 +0900

    sss

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTemplate from '../components/common/PageTemplate';
+
+
+const MainPage = () => {
+  return (
+    <PageTemplate>
+        
+    </PageTemplate>
+  );
+};
+
+export default MainPage;
